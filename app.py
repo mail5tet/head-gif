2159342a1cf4980875597e404d9721b4d77f3b1c
mail5tet <mail5tet@proton.me>
2025-07-23 22:58:08 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 
-from flask import Flask, request, make_response, jsonify, Response, send_from_directory
+from flask import app, request, make_response, jsonify, Response, send_from_directory
 import base64
 import datetime
 import requests
